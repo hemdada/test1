5dac220dca759136caf1f318dd445d3111cf2d4b
Hemanth Reddy <hemanth.sannareddy@wavemaker.com>
2017-03-03 17:14:56 +0530

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "test1",
+  "homePage" : "Main",
+  "name" : "test1",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
